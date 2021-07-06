8b5e119eb429de177cb5d0014561e3af77252ed1
sharonrosa <srwijnberg@gmail.com>
2021-07-06 16:13:43 +0200

    write script to configure .env

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log("hello")
+import readline from "readline-sync";
+import fs from "fs";
+
+const APIkey: string = readline.question("Plak je API key hier:")
+const DBurl: string = readline.question("Plak hier je database url:")
+
+console.log({APIkey, DBurl})
+
+fs.writeFileSync(".env", `
+API_KEY=${APIkey}
+DB_URL=${DBurl}
+`)
+
